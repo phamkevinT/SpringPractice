0a8ee6f51c83a8c49a5a9b0e1ded9e2105f20b5d
Kevin Pham <phambamkevin@gmail.com>
2021-01-22 20:44:53 -0800

    Edit spacing

--- spring-demo-one/src/com/kevinpham/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/kevinpham/springdemo/BaseballCoach.java
@@ -4,9 +4,11 @@ public class BaseballCoach implements Coach {
 
 	@Override
 	public String getDailyWorkout() {
+		
 		return "Spend 30 minutes on batting practice.";
 	}
 	
 	
 	
 }
+
--- spring-demo-one/src/com/kevinpham/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/kevinpham/springdemo/TrackCoach.java
@@ -4,6 +4,7 @@ public class TrackCoach implements Coach {
 
 	@Override
 	public String getDailyWorkout() {
+		
 		return "Run a hard 5k.";
 	}
 
