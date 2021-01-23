8415eeb57aa3019dda7d041b6bff4c62cb8c87f5
Kevin Pham <phambamkevin@gmail.com>
2021-01-22 20:06:19 -0800

    Created two types of coaches

---
+++ spring-demo-one/src/com/kevinpham/springdemo/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.kevinpham.springdemo;
+
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting practice.";
+	}
+	
+	
+	
+}
