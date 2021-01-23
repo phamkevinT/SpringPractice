ca7b89c63d45534687f8f0af54376d71ef70f823
Kevin Pham <phambamkevin@gmail.com>
2021-01-22 21:32:15 -0800

    Configured the dependency injection in Spring config file

--- spring-demo-one/src/com/kevinpham/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/kevinpham/springdemo/BaseballCoach.java
@@ -19,7 +19,9 @@ public class BaseballCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		return null;
+		
+		// Use for my fortuneService to get a fortune
+		return fortuneService.getFortune();
 	}
 	
 	
