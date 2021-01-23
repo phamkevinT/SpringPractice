6f7acc353292a4e4b1ca4768446430aae7674fcf
Kevin Pham <phambamkevin@gmail.com>
2021-01-22 21:36:10 -0800

    Temp Fixed Error

--- spring-demo-one/src/com/kevinpham/springdemo/MyApp.java
+++ spring-demo-one/src/com/kevinpham/springdemo/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		// Create the object
-		Coach theCoach = new TrackCoach();
+		Coach theCoach = new BaseballCoach(null);
 		
 		// Use the object
 		System.out.println(theCoach.getDailyWorkout());
