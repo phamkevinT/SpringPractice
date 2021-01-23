aa361b008155cbc2f7aa1b15f22f608d5e2b4fad
Kevin Pham <phambamkevin@gmail.com>
2021-01-22 21:43:56 -0800

    Created no argument constructor for coaches

--- spring-demo-one/src/com/kevinpham/springdemo/MyApp.java
+++ spring-demo-one/src/com/kevinpham/springdemo/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		// Create the object
-		Coach theCoach = new BaseballCoach();
+		Coach theCoach = new TrackCoach();
 		
 		// Use the object
 		System.out.println(theCoach.getDailyWorkout());
