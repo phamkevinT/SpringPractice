74ee6cc342150d324ff284b34fbb5ed593130635
Kevin Pham <phambamkevin@gmail.com>
2021-01-22 21:24:51 -0800

    Constructor for dependency injection

--- spring-demo-one/src/com/kevinpham/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/kevinpham/springdemo/BaseballCoach.java
@@ -2,10 +2,24 @@ package com.kevinpham.springdemo;
 
 public class BaseballCoach implements Coach {
 
+	// Define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	// Define a constructor for dependency injection
+	public BaseballCoach(FortuneService theFortuneService) {
+		this.fortuneService = theFortuneService;
+	}
+	
+	
 	@Override
 	public String getDailyWorkout() {
 		
 		return "Spend 30 minutes on batting practice.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return null;
 	}
 	
 	
--- spring-demo-one/src/com/kevinpham/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/kevinpham/springdemo/TrackCoach.java
@@ -2,10 +2,23 @@ package com.kevinpham.springdemo;
 
 public class TrackCoach implements Coach {
 
+	// Define a private field for the dependency
+	private FortuneService fortuneService;
+
+	// Define a constructor for dependency injection
+	public TrackCoach(FortuneService theFortuneService) {
+		this.fortuneService = theFortuneService;
+	}
+
 	@Override
 	public String getDailyWorkout() {
-		
+
 		return "Run a hard 5k.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return null;
 	}
 
 }
