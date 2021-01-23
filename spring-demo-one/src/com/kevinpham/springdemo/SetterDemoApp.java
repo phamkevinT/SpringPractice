6cd055d4657a60044dbc024f33c8038fae7578ab
Kevin Pham <phambamkevin@gmail.com>
2021-01-22 22:03:37 -0800

    Created new class to demo setter injection

---
+++ spring-demo-one/src/com/kevinpham/springdemo/SetterDemoApp.java
@@ -0,0 +1,16 @@
+package com.kevinpham.springdemo;
+
+
+/**
+ * Demo of the Setter Injection
+ * @author Kevin Pham
+ *
+ */
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
