da1a742d9b61595536df038d400df2d131e4ede4
Kevin Pham <phambamkevin@gmail.com>
2021-01-24 15:55:56 -0800

    Added tests and comments

--- spring-demo-one/src/com/kevinpham/springdemo/SetterDemoApp.java
+++ spring-demo-one/src/com/kevinpham/springdemo/SetterDemoApp.java
@@ -15,6 +15,9 @@ public class SetterDemoApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// Retrieve the bean from spring container
+		// We use the CricketCoach class instead of the Coach interface because in this
+		// scenario, we need the additional methods of getEmailAddress() and getTeam()
+		// that CricketCoach provides. Coach only has getDailyWorkout() and getDailyFortune().
 		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
 		// Call methods on the bean
