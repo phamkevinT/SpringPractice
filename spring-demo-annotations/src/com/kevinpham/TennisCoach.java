a6595b7915addedc4601d54f9d9a1e7efcf1fa6a
Kevin Pham <phambamkevin@gmail.com>
2021-02-15 22:27:13 -0800

    Qualifiers for Dependency Injection

--- spring-demo-annotations/src/com/kevinpham/TennisCoach.java
+++ spring-demo-annotations/src/com/kevinpham/TennisCoach.java
@@ -1,13 +1,16 @@
 package com.kevinpham;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
-//	// Field Injection
-//	@Autowired
+	// Field Injection
+	// Qualifier specifies which bean to use
+	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
 	// Set up default constructor
