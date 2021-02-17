39e17b0a4425018c0582b54d8c2347127eecde6f
Kevin Pham <phambamkevin@gmail.com>
2021-02-16 16:08:46 -0800

    Spring Config with Java Code - No XML

---
+++ spring-demo-annotations/src/com/kevinpham/SportConfig.java
@@ -0,0 +1,10 @@
+package com.kevinpham;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.kevinpham")
+public class SportConfig {
+	
+}
