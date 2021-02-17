2a79be414391b1ccae83d3c89505bfb50637a85e
Kevin Pham <phambamkevin@gmail.com>
2021-02-17 13:01:22 -0800

    Binding Request Param

--- spring-mvc-demo/src/com/kevinpham/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/kevinpham/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -25,7 +26,7 @@ public class HelloWorldController {
 	@RequestMapping("/processFormVersionTwo")
 	public String letsShoutDude(HttpServletRequest request, Model model) {
 
-		// Read the request parameter from the HTML form 
+		// Read the request parameter from the HTML form
 		// (studentName = form input name)
 		String theName = request.getParameter("studentName");
 
@@ -35,7 +36,25 @@ public class HelloWorldController {
 		// Create the message
 		String result = "Yo! " + theName;
 
-		// Add the message to the model 
+		// Add the message to the model
+		// (message = the ID to retrieve the result in helloworld.jsp)
+		model.addAttribute("message", result);
+
+		return "helloworld";
+	}
+
+	// New controller method to read form data and add data to the model using
+	// '@RequestParam' in the parameter
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+		// Convert the data to all UpperCase
+		theName = theName.toUpperCase();
+
+		// Create the message
+		String result = "Hello my friend, " + theName;
+
+		// Add the message to the model
 		// (message = the ID to retrieve the result in helloworld.jsp)
 		model.addAttribute("message", result);
 
