56bcda9707fc213910989568d49193a5c470b19a
Kevin Pham <phambamkevin@gmail.com>
2021-02-19 15:46:35 -0800

    Spring MVC Form Validation

--- spring-mvc-demo/src/com/kevinpham/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/kevinpham/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.kevinpham.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -9,8 +11,12 @@ public class Customer {
 	private String firstName;
 	
 	@NotNull(message="is required")
-	@Size(min=1)
+	@Size(min=1, message="is required")
 	private String lastName;
+	
+	@Min(value=0, message="Must be greater than or equal to 0.")
+	@Max(value=10, message="Must be less than or equal to 10.")
+	private int freePasses;
 	
 	
 	public String getFirstName() {
@@ -25,5 +31,13 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	public int getFreePasses() {
+		return freePasses;
+	}
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+	
+	
 	
 }
--- spring-mvc-demo/src/com/kevinpham/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/kevinpham/springdemo/mvc/CustomerController.java
@@ -1,12 +1,34 @@
 package com.kevinpham.springdemo.mvc;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	
+	// Add initbuilder ... to convert trim input strings
+	// remove leading and trailing white spaces
+	// resolve issue for our validation
+	
+	@InitBinder
+	public void initBuilder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
+	
+	
 
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
@@ -14,5 +36,17 @@ public class CustomerController {
 		theModel.addAttribute("customer", new Customer());
 		
 		return "customer-form";
+	}
+	
+	
+	@RequestMapping("/processForm") 
+	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+		
+		if(theBindingResult.hasErrors()) {
+			return "customer-form";
+		}
+		else {
+			return "customer-confirmation";
+		}
 	}
 }
