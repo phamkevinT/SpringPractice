a948ec7263713eb3ab5dff9e0eb12ea614dcec5a
Kevin Pham <phambamkevin@gmail.com>
2021-02-21 18:18:07 -0800

    Querying from database

--- hibernate-tutorial/src/com/kevinpham/hibernate/demo/QueryStudentDemo.java
+++ hibernate-tutorial/src/com/kevinpham/hibernate/demo/QueryStudentDemo.java
@@ -1,5 +1,7 @@
 package com.kevinpham.hibernate.demo;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -21,17 +23,39 @@ public class QueryStudentDemo {
 
 		try {
 
-			// Create student object
-			System.out.println("Creating a new student object...");
-			Student tempStudent = new Student("Kevin", "Pham", "kevin@pham.com");
-
 			// Start a transaction
 			session.beginTransaction();
 
-			// Save the student object
-			System.out.println("Saving the student object...");
-			session.save(tempStudent);
+			// Query students
+			List<Student> theStudents = session.createQuery("from Student").getResultList();
 			
+					// Display the students
+					displayStudents(theStudents);
+			
+			// Query students: lastName='Doe'
+			theStudents = session.createQuery("from Student s where s.lastname='Doe'").getResultList();
+			
+					// Display the students
+					System.out.println("\n\nStudents who have the last name of 'Doe'");
+					displayStudents(theStudents);
+			
+			// Query students: lastName='Doe' or firstName='Daffy'
+			theStudents = session.createQuery("from Student s where s.lastname='Doe' OR s.firstName='Daffy'").getResultList();
+			
+					// Display the students
+					System.out.println("\n\nStudents who have the last name of 'Doe' or first name of 'Daffy'");
+					displayStudents(theStudents);
+			
+			// Query students: where email LIKE '%pham.com'
+			theStudents = session.createQuery("FROM Student s WHERE s.email LIKE '%pham.com'").getResultList();
+			
+					// Display the students
+					System.out.println("\n\nStudents who have the email ending in 'pham.com'");
+					displayStudents(theStudents);
+					
+					
+					
+					
 			// Commit transaction
 			session.getTransaction().commit();
 
@@ -42,6 +66,12 @@ public class QueryStudentDemo {
 			factory.close();
 		}
 
+	}
+
+	private static void displayStudents(List<Student> theStudents) {
+		for(Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
 	}
 
 }
